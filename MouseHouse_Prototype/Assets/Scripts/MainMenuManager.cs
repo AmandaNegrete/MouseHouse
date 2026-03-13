e6f3538b1fcea95201a21c6a8f00250b32529bcc
RJWMD <113476572+RJWMD@users.noreply.github.com>
2026-03-13 17:18:00 -0400

    added level select and main menu scene

--- MouseHouse_Prototype/Assets/Scripts/MainMenuManager.cs
+++ MouseHouse_Prototype/Assets/Scripts/MainMenuManager.cs
@@ -12,12 +12,30 @@ public class MainMenuManager : MonoBehaviour
 
     InputAction startAction;
 
+    public CanvasGroup levelSelect;
+
+    public Transform levelsContainer;
+
+    public GameObject LevelSelectListingPrefab;
+
     private void Start()
     {
         startAction = inputs.actions["jump"];
 
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
+        PopulateLevelSelect();
+    }
+
+    void PopulateLevelSelect()
+    {
+        for(int i = 0; i < levelSceneNames.Count; i++)
+        {
+            GameObject addedListing = Instantiate(LevelSelectListingPrefab, levelsContainer);
+            LevelListingButton listing = addedListing.GetComponent<LevelListingButton>();
+            listing.text.text = "" + (i + 1);
+            listing.LevelName = levelSceneNames[i];
+        }
     }
 
     public void LoadNextLevel()
@@ -25,14 +43,28 @@ public class MainMenuManager : MonoBehaviour
         SceneManager.LoadScene(levelSceneNames[0]);
     }
 
+    public void OpenLevelSelect()
+    {
+        levelSelect.alpha = 1;
+        levelSelect.blocksRaycasts = true;
+        levelSelect.interactable = true;
+    }
+
+    public void CloseLevelSelect()
+    {
+        levelSelect.alpha = 0;
+        levelSelect.blocksRaycasts = false;
+        levelSelect.interactable = false;
+    }
+
     private void Update()
     {
         //Change this to be keyboard navigation. Should default selection to start.
         //Also should have level select at some point?
-        if(startAction.WasPressedThisFrame())
-        {
-            LoadNextLevel();
-        }
+        //if(startAction.WasPressedThisFrame())
+        //{
+        //    LoadNextLevel();
+        //}
         
     }
 }
