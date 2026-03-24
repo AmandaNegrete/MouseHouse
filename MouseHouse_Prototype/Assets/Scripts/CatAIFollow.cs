1534a2e64061a007142204d3739c11a9fd9a64fb
Amanda <anegret1@umbc.edu>
2026-03-24 13:37:29 -0400

    More Additions/Edits

--- MouseHouse_Prototype/Assets/Scripts/CatAIFollow.cs
+++ MouseHouse_Prototype/Assets/Scripts/CatAIFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 public class CatAIFollow : MonoBehaviour
 {
     // Radius/Sensing variables
-    private const float detectionRadius = 4f;
+    private const float detectionRadius = 6f;
     private const float catnipRadius = 3f;
     private const float roamRadius = 30f;
     private const float flipThreshold = 0.05f;
