7a75a8325bae535b8554bb8a3587f6cc105baae6
mlmoore2259 <mlmoore2259@gmail.com>
2026-04-13 20:17:10 -0400

    Started work on the interaction indicator shader and manager.

---
+++ MouseHouse_Prototype/Assets/Scripts/IndicatorManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class IndicatorManager : MonoBehaviour
+{
+    public Shader interactableShader;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //public void ApplyShader(GameObject obj)
+    //{
+    //    Renderer[] renderers = obj.GetComponentsInChildren<Renderer>();
+    //    foreach (Renderer renderer in renderers)
+    //    {
+            
+    //    }
+    //}
+}
