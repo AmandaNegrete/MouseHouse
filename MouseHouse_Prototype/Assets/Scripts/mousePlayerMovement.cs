0567f3d1c78c83d12555ba28923986f890eccfe1
RJWMD <113476572+RJWMD@users.noreply.github.com>
2026-04-28 18:56:37 -0400

    Fixed bobbing with controller, added nonshiny material, added lights to scene, untagged some incorrectly tagged items

--- MouseHouse_Prototype/Assets/Scripts/mousePlayerMovement.cs
+++ MouseHouse_Prototype/Assets/Scripts/mousePlayerMovement.cs
@@ -210,7 +210,7 @@ public class PlayerMovement : MonoBehaviour
 
 
         //Bobbing anim
-        float moveMagnitude = (moveInput * moveSpeed).y;
+        float moveMagnitude = (moveInput * moveSpeed).magnitude;
         currBobbingIntensity = Mathf.SmoothDamp(currBobbingIntensity, moveMagnitude * bobbingIntensity, ref bobVel, .2f);
         if (!Mouseground)
             currBobbingIntensity = 0;
